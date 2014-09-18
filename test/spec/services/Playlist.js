88726cb6137237606e9e9bec7c4cb03a91001479
antm611 <antm88@gmail.com>
2014-09-18 19:08:44 +0100

    Added some Playlist tests

---
+++ test/spec/services/Playlist.js
@@ -0,0 +1,312 @@
+'use strict';
+
+describe('Service: Playlist', function() {
+
+    var service,
+        $q,
+        $rootScope,
+        ApiRequest;
+
+    beforeEach(function() {
+        module('musicServerApp');
+
+        inject(function($injector) {
+            $q = $injector.get('$q');
+            $rootScope = $injector.get('$rootScope');
+            ApiRequest = $injector.get('ApiRequest');
+
+            service = $injector.get('Playlist', {
+                $q: $q,
+                ApiRequest: ApiRequest
+            });
+        });
+    });
+
+    describe('Initialisation', function() {
+        it('should initialise the trackArray', function() {
+            expect(service.trackArray).toEqual([]);
+        });
+    });
+
+    describe('addTrack', function() {
+        it('should add a new track to an empty track array', function() {
+            var mockTrack = {
+                Name: 'aasdgbsadg9sag8fubigas'
+            };
+            service.addTrack(mockTrack, 0);
+
+            expect(service.trackArray.length).toBe(1);
+            expect(service.trackArray[0].Name).toBe('aasdgbsadg9sag8fubigas');
+        });
+
+        it('should add a new track to the end of a non-empty track array if the position is negative', function() {
+            service.trackArray = [{}];
+            var mockTrack = {
+                Name: 'sdaf9sh8fhs9af8suof'
+            };
+            service.addTrack(mockTrack, -1);
+
+            expect(service.trackArray.length).toBe(2);
+            expect(service.trackArray[1].Name).toBe('sdaf9sh8fhs9af8suof');
+        });
+
+        it('should add a new track to the position specified in a non-empty track array', function() {
+            service.trackArray = [{}, {}];
+            var mockTrack = {
+                Name: 'asdgsad8hg9as8dhisdo'
+            };
+            service.addTrack(mockTrack, 1);
+
+            expect(service.trackArray.length).toBe(3);
+            expect(service.trackArray[1].Name).toBe('asdgsad8hg9as8dhisdo');
+        });
+
+        it('should create a copy of the track added, and not modify the original track', function() {
+            service.trackArray = [];
+            var mockTrack = {
+                Name: 'asdfsad90f7gsad8f7gsiuh',
+                RandomUnusedProperty: 'asdghasg0hsdgh'
+            };
+            service.addTrack(mockTrack, -1);
+
+            expect(service.trackArray.length).toBe(1);
+            expect(service.trackArray[0].Name).toBe('asdfsad90f7gsad8f7gsiuh');
+            expect(service.trackArray[0].RandomUnusedProperty).toBe('asdghasg0hsdgh');
+            expect(service.trackArray[0]).not.toBe(mockTrack);
+        });
+
+        it('should submit a conversion request for the new track if the playlist length is 0', function() {
+            service.trackArray = [];
+            var mockTrack = {
+                ID: 16437,
+                Name: 'asduasg087asdfiusdabfjds'
+            };
+            spyOn(ApiRequest.track, 'convert').andCallFake(function() {
+                return {
+                    submit: function() {
+                        return $q.when({
+                            Result: 'Success',
+                            FileName: 'asdfus9fbsaufi'
+                        });
+                    }
+                };
+            });
+
+            service.addTrack(mockTrack, -1);
+            expect(ApiRequest.track.convert).toHaveBeenCalledWith(16437);
+            $rootScope.$digest();
+
+            expect(service.trackArray.length).toBe(1);
+            expect(service.trackArray[0].Name).toBe('asduasg087asdfiusdabfjds');
+            expect(service.trackArray[0].FileName).toBe('asdfus9fbsaufi');
+        });
+
+        it('should submit a conversion request for the new track if the playlist length is 0 and log a console warning if an error occurs', function() {
+            service.trackArray = [];
+            var mockTrack = {
+                ID: 15987,
+                Name: 'asdf9a8sgf98asdfh9saphoij'
+            };
+            spyOn(console, 'warn');
+            spyOn(ApiRequest.track, 'convert').andCallFake(function() {
+                return {
+                    submit: function() {
+                        return $q.reject('asdofuasdf9gsadfubsadiyfugho');
+                    }
+                };
+            });
+
+            service.addTrack(mockTrack, -1);
+            $rootScope.$digest();
+
+            expect(service.trackArray.length).toBe(1);
+            expect(service.trackArray[0].Name).toBe('asdf9a8sgf98asdfh9saphoij');
+            expect(service.trackArray[0].FileName).toBeUndefined();
+            expect(console.warn).toHaveBeenCalledWith('asdofuasdf9gsadfubsadiyfugho');
+        });
+
+        it('should only allow one conversion request to be submitted for a track at a time', function() {
+            service.trackArray = [];
+            var mockTrack = {
+                ID: 16437,
+                Name: 'asduasg087asdfiusdabfjds'
+            };
+            spyOn(ApiRequest.track, 'convert').andCallFake(function() {
+                return {
+                    submit: function() {
+                        return $q.defer().promise;
+                    }
+                };
+            });
+
+            service.addTrack(mockTrack, -1);
+            expect(service.trackArray.length).toBe(1);
+            var newTrack = service.trackArray[0];
+            service.trackArray.length = 0;
+            service.addTrack(newTrack);
+            $rootScope.$digest();
+
+            expect(ApiRequest.track.convert.callCount).toBe(1);
+        });
+    });
+
+    describe('addTracks', function() {
+        it('should call addTrack for every track in the array', function() {
+            service.trackArray = [];
+            var mockTracks = [{
+                Name: 'Track1'
+            }, {
+                Name: 'Track2'
+            }, {
+                Name: 'Track3'
+            }, {
+                Name: 'Track4'
+            }];
+            spyOn(service, 'addTrack');
+
+            service.addTracks(mockTracks);
+
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[0], 0);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[1], 1);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[2], 2);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[3], 3);
+            expect(service.addTrack.callCount).toBe(4);
+        });
+
+        it('should add the tracks after a given track in the array', function() {
+            service.trackArray = [{}, {}];
+            var mockTracks = [{
+                Name: 'Track1'
+            }, {
+                Name: 'Track2'
+            }, {
+                Name: 'Track3'
+            }, {
+                Name: 'Track4'
+            }];
+            spyOn(service, 'addTrack');
+
+            service.addTracks(mockTracks, service.trackArray[0], true);
+
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[0], 1);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[1], 2);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[2], 3);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[3], 4);
+            expect(service.addTrack.callCount).toBe(4);
+        });
+
+        it('should add the tracks before a given track in the array', function() {
+            service.trackArray = [{}, {}];
+            var mockTracks = [{
+                Name: 'Track1'
+            }, {
+                Name: 'Track2'
+            }, {
+                Name: 'Track3'
+            }, {
+                Name: 'Track4'
+            }];
+            spyOn(service, 'addTrack');
+
+            service.addTracks(mockTracks, service.trackArray[1], false);
+
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[0], 1);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[1], 2);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[2], 3);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[3], 4);
+            expect(service.addTrack.callCount).toBe(4);
+        });
+
+        it('should add the tracks at the end if an unknown relativeTrack is specified', function() {
+            service.trackArray = [{}, {}];
+            var mockTracks = [{
+                Name: 'Track1'
+            }, {
+                Name: 'Track2'
+            }, {
+                Name: 'Track3'
+            }, {
+                Name: 'Track4'
+            }];
+            spyOn(service, 'addTrack');
+
+            service.addTracks(mockTracks, {}, true);
+
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[0], 2);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[1], 3);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[2], 4);
+            expect(service.addTrack).toHaveBeenCalledWith(mockTracks[3], 5);
+            expect(service.addTrack.callCount).toBe(4);
+        });
+    });
+
+    describe('addTracksByAlbum', function() {
+        it('should load the results from the API and add them with the addTracks function', function() {
+            service.trackArray = [];
+            var mockTracks = [{
+                Name: 'Track 1'
+            }, {
+                Name: 'Track 2'
+            }];
+            spyOn(service, 'addTracks');
+            spyOn(ApiRequest.track, 'getFromAlbum').andCallFake(function() {
+                return {
+                    submit: function() {
+                        return $q.when(mockTracks);
+                    }
+                };
+            });
+
+            service.addTracksByAlbum(34983);
+            $rootScope.$digest();
+
+            expect(ApiRequest.track.getFromAlbum).toHaveBeenCalledWith(34983);
+            expect(service.addTracks).toHaveBeenCalledWith(mockTracks);
+        });
+    });
+
+    describe('addTracksByArtist', function() {
+        it('should load the results from the API and add them with the addTracks function', function() {
+            service.trackArray = [];
+            var mockTracks = [{
+                Name: 'Track 1'
+            }, {
+                Name: 'Track 2'
+            }];
+            spyOn(service, 'addTracks');
+            spyOn(ApiRequest.track, 'getFromAlbum').andCallFake(function() {
+                return {
+                    submit: function() {
+                        return $q.when(mockTracks);
+                    }
+                };
+            });
+
+            service.addTracksByAlbum(235235);
+            $rootScope.$digest();
+
+            expect(ApiRequest.track.getFromAlbum).toHaveBeenCalledWith(235235);
+            expect(service.addTracks).toHaveBeenCalledWith(mockTracks);
+        });
+    });
+
+    describe('getTrack', function() {
+        
+    });
+
+    describe('removeTrack', function() {
+
+    });
+
+    describe('removeTracks', function() {
+
+    });
+
+    describe('clear', function() {
+
+    });
+
+    describe('deselectAll', function() {
+
+    });
+});
