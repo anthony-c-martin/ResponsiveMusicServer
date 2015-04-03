48969940c21bd1d73c7ef8ec6f1985513b251f13
antm611 <antm88@gmail.com>
2015-04-03 15:45:38 +0100

    Updated app module to be independent of mock api

--- app/scripts/app.init.js
+++ app/scripts/app.init.js
@@ -2,18 +2,6 @@
     'use strict';
 
     angular.element(document).ready(function() {
-        function addModuleIfPresent(modules, module) {
-            try {
-                angular.module(module);
-                modules.push(module);
-            } catch (e) {
-
-            }
-        }
-
-        var modules = ['app'];
-        addModuleIfPresent(modules, 'mock.api');
-
-        angular.bootstrap(document.querySelector('[data-main-app]'), modules, {});
+        angular.bootstrap(document.querySelector('[data-main-app]'), ['app'], {});
     });
 })();
